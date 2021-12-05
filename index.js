4c0908fbe0ae6561431fc8d53be2c9cd68e9648f
evlon <liamgnc@gmail.com>
2021-12-05 13:21:45 +0000

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const Fastify = require('fastify')
+const server = Fastify()
+const proxy = require('fastify-http-proxy')
+
+// /api/x will be proxied to http://my-api.example.com/x
+server.register(proxy, {
+  upstream: 'https://hw.p0q.top/',
+  
+  http2: false // optional
+})
+
+server.listen(3000)
