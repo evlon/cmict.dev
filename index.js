ec3949839dd3ffb6379b49046d44f1d23b360ed3
evlon <liamgnc@gmail.com>
2021-12-05 21:57:44 +0800

    update upstream

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const proxy = require('fastify-http-proxy')
 
 // /api/x will be proxied to http://my-api.example.com/x
 server.register(proxy, {
-  upstream: 'https://hw.p0q.top/',
+  upstream: 'https://cmict.dev.p0q.top/',
   
   http2: false // optional
 })
